31080a7bfc2cf98d5fafccbe141e9adc7363a36a
Swadesh Vishwakarma <swadesh06082005@gmail.com>
2026-07-20 11:05:14 +0530

    Day X: Small update

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_db',
+        'USER': 'root',
+        'PASSWORD': 'Soot@123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
